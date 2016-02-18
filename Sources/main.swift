009913120c8ea598c81a5acb78b9bb24b85a0da5
Elliott Minns <elliott.minns@me.com>
2016-02-18 22:44:11 +0000

    Updated main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -8,7 +8,7 @@ app.use(path: "/", controller: IndexController())
 
 app.listen(port: 4000) { error in
     if error == nil {
-        print("App listening on port 4000")
+        print("App listening on port \(app.port)")
     } else {
         print(error)
     }
