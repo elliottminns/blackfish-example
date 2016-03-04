56423ef2d05ce00e774902bb22e59fd0dd812e5b
Elliott Minns <elliott.minns@me.com>
2016-03-04 19:10:08 +0000

    Updated with Makefile

--- Sources/IndexController.swift
+++ Sources/IndexController.swift
@@ -3,10 +3,15 @@ import Blackfish
 class IndexController: Controller {
     
     func routes(router: Router) {
-        router.get("/", handler: index);
+        router.get("/", handler: index)
+        router.get("/test", handler: test)
     }
 
     func index(request: Request, response: Response) {
         response.render("welcome.stencil")
+    }
+
+    func test(request: Request, response: Response) {
+        response.render("test.stencil", data: ["names": [["first": "John", "last": "Smith"], ["first": "Alec", "last": "Baldwin"]]])
     }
 }
